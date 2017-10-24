5aced8aec2fc2fabc0948bfd3f29bbb79efcb8f8
crisalmendra <crisalmendra2002@yahoo.com.ar>
2017-10-24 11:59:28 -0300

    disfraces pulman

--- Disfraces/DisfrazFunction.js
+++ Disfraces/DisfrazFunction.js
@@ -1,3 +1,3 @@
-var disfraces=["unicornio","Alice in Wonderland","Pikachu", "Hello Kitty", "Charlotte Caniggia", "Alex Caniggia", "Gogo Yubari", "britney en 2007", "orejas de tigre", "Doctora", "coneja", "Cleopatra", "estudiante de slytherin", "Leia", "Tina Turner", "La minita del bebé de Rosemary"];
+var disfraces=["unicornio","Alice in Wonderland","Pikachu", "Hello Kitty", "Charlotte Caniggia", "Alex Caniggia", "Gogo Yubari", "britney en 2007", "orejas de tigre", "Doctora", "coneja", "Cleopatra", "estudiante de slytherin", "Leia", "Tina Turner", "La minita del bebé de Rosemary","merlina Adams", "Wally", "super girl", "moana", "semaforo", "anana", "abeja"];
 
 
