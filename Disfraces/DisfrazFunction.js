e3e1697f4f886b0c9588b3d8c36a91b6f79c6f53
macavilla <maca.villa.a@gmail.com>
2017-10-24 11:29:15 -0300

    maca

--- Disfraces/DisfrazFunction.js
+++ Disfraces/DisfrazFunction.js
@@ -1,3 +1,3 @@
-var disfraces=["unicornio","Alice in Wonderland","Pikachu", "Hello Kitty", "Charlotte Caniggia", "Alex Caniggia", "Gogo Yubari", "britney en 2007", "orejas de tigre", "Doctora", "coneja", "Cleopatra", "estudiante de slytherin", "Leia", "Tina Turner"];
+var disfraces=["unicornio","Alice in Wonderland","Pikachu", "Hello Kitty", "Charlotte Caniggia", "Alex Caniggia", "Gogo Yubari", "britney en 2007", "orejas de tigre", "Doctora", "coneja", "Cleopatra", "estudiante de slytherin", "Leia", "Tina Turner", "La minita del bebé de Rosemary"];
 
 
