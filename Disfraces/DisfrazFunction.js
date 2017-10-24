fa2581d0e09f1759e46f0ac829d82f306f4c7d77
tamyt <tamitugues@gmail.com>
2017-10-24 11:38:02 -0300

    disfraces

--- Disfraces/DisfrazFunction.js
+++ Disfraces/DisfrazFunction.js
@@ -1,3 +1,3 @@
-var disfraces=["unicornio","Alice in Wonderland","Pikachu", "Hello Kitty", "Charlotte Caniggia", "Alex Caniggia", "Gogo Yubari", "britney en 2007", "orejas de tigre", "Doctora", "coneja", "Cleopatra", "estudiante de slytherin", "Leia", "Tina Turner", "La minita del bebé de Rosemary"];
+var disfraces=["Unicornio","Alice in Wonderland","Pikachu", "Hello Kitty", "Charlotte Caniggia", "Momia", "Gogo Yubari", "britney en 2007", "orejas de tigre", "Doctora", "coneja", "Cleopatra", "estudiante de slytherin", "Leia", "Tina Turner", "La minita del bebé de Rosemary","11 de stranger things","kill bill","Raven de teen titans"];
 
 
